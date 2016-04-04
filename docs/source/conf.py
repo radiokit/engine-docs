0dd5a20603e69a3fb239555795b06389a7355dad
Marcin Lewandowski <marcin@radiokit.org>
2016-04-04 16:30:48 +0200

    Fixed man page name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -258,7 +258,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'radiokitengine', u'RadioKit Engine Documentation',
+    (master_doc, 'radiokit-engine', u'RadioKit Engine Documentation',
      [author], 1)
 ]
 
